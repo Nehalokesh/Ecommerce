dcec904bf9752c758d3cd85139454992a1671454
nehal19697@gmail.com <nehal19697@gmail.com>
2022-10-24 13:41:15 +0530

    task11 commit

--- store.js
+++ store.js
@@ -59,13 +59,19 @@ if (e.target.className=='purchase-btn'){
     document.querySelector('.cart-number').innerText = 0
     document.querySelector('#total-value').innerText = `0`;
 }
-})
 
-// if (e.target.innerText=='REMOVE'){
-//     let total_cart_price = document.querySelector('#total-value').innerText;
-//     total_cart_price = parseFloat(total_cart_price).toFixed(2) - parseFloat(document.querySelector(`#${e.target.parentNode.parentNode.id} .cart-price`).innerText).toFixed(2) ;
-//     document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)-1
-//     document.querySelector('#total-value').innerText = `${total_cart_price.toFixed(2)}`
-//     e.target.parentNode.parentNode.remove()
-// }
-// })
+if (e.target.innerText=='REMOVE'){
+    let total_cart_price = document.querySelector('#total-value').innerText;
+    total_cart_price = parseFloat(total_cart_price).toFixed(2) - parseFloat(document.querySelector(`#${e.target.parentNode.parentNode.id} .cart-price`).innerText).toFixed(2) ;
+    document.querySelector('.cart-number').innerText = parseInt(document.querySelector('.cart-number').innerText)-1
+    document.querySelector('#total-value').innerText = `${total_cart_price.toFixed(2)}`
+    e.target.parentNode.parentNode.remove()
+}
+})
+document.addEventListener('DOMContentLoaded',()=>{
+    axios.get('http://localhost:3000/products')
+    .then((data) =>{
+        console.log(data);
+
+    })
+})
